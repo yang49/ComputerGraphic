da346d1815f49a5d1cc2b4b824ac8460630cfcc7
Yang <y655li@uwaterloo.ca>
2016-10-08 15:35:24 -0400

    init commit

---
+++ A4/A4.cpp
@@ -0,0 +1,56 @@
+#include <glm/ext.hpp>
+
+#include "A4.hpp"
+
+void A4_Render(
+		// What to render
+		SceneNode * root,
+
+		// Image to write to, set to a given width and height
+		Image & image,
+
+		// Viewing parameters
+		const glm::vec3 & eye,
+		const glm::vec3 & view,
+		const glm::vec3 & up,
+		double fovy,
+
+		// Lighting parameters
+		const glm::vec3 & ambient,
+		const std::list<Light *> & lights
+) {
+
+  // Fill in raytracing code here...
+
+  std::cout << "Calling A4_Render(\n" <<
+		  "\t" << *root <<
+          "\t" << "Image(width:" << image.width() << ", height:" << image.height() << ")\n"
+          "\t" << "eye:  " << glm::to_string(eye) << std::endl <<
+		  "\t" << "view: " << glm::to_string(view) << std::endl <<
+		  "\t" << "up:   " << glm::to_string(up) << std::endl <<
+		  "\t" << "fovy: " << fovy << std::endl <<
+          "\t" << "ambient: " << glm::to_string(ambient) << std::endl <<
+		  "\t" << "lights{" << std::endl;
+
+	for(const Light * light : lights) {
+		std::cout << "\t\t" <<  *light << std::endl;
+	}
+	std::cout << "\t}" << std::endl;
+	std:: cout <<")" << std::endl;
+
+	size_t h = image.height();
+	size_t w = image.width();
+
+	for (uint y = 0; y < h; ++y) {
+		for (uint x = 0; x < w; ++x) {
+			// Red: increasing from top to bottom
+			image(x, y, 0) = (double)y / h;
+			// Green: increasing from left to right
+			image(x, y, 1) = (double)x / w;
+			// Blue: in lower-left and upper-right corners
+			image(x, y, 2) = ((y < h/2 && x < w/2)
+						  || (y >= h/2 && x >= w/2)) ? 1.0 : 0.0;
+		}
+	}
+
+}
